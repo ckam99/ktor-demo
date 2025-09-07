8454d3aa68d6b35e4e4bcef32c62da77256a5791
ckam99 <ckam225@gmail.com>
2025-09-07 21:22:13 +0000

    basic request handling

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,13 +1,35 @@
 package com.example
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
+import io.ktor.server.request.receiveText
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+
+
     routing {
+
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("/greet/{name}"){
+            val name = call.pathParameters["name"]
+            if (name == null) {
+                call.respond(HttpStatusCode.BadRequest)
+                return@get
+            }
+            call.respondText("Hello, $name")
+        }
+
+        post("/greet") {
+            val name = call.receiveText()
+            var message = "Hello, $name"
+            val age = call.queryParameters["age"]
+            age?.let { message += ", you are $age old" }
+            call.respondText(message)
         }
     }
 }
