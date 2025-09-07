d20bee84679137615139797aa66e373d498146e1
ckam99 <ckam225@gmail.com>
2025-09-07 22:36:28 +0000

    Serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 group = "com.example"
@@ -16,4 +17,7 @@ dependencies {
     implementation(libs.logback.classic)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
+
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
 }
--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -1,6 +1,10 @@
 package com.example
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class User(
+    var id: Int ? = null,
     val name: String,
     val email: String,
 )
