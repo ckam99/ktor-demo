1a253cc50c59aa94996169e4978d2b97b2fa6db6
ckam99 <ckam225@gmail.com>
2025-09-09 18:47:24 +0000

    try to resolve flyway errors

--- src/main/kotlin/plugins/Databases.kt
+++ src/main/kotlin/plugins/Databases.kt
@@ -4,6 +4,8 @@ import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.application.Application
 import org.flywaydb.core.Flyway
+import org.flywaydb.core.api.FlywayException
+import org.flywaydb.core.api.exception.FlywayValidateException
 import org.jetbrains.exposed.sql.Database
 import org.jooq.DSLContext
 import org.jooq.SQLDialect
@@ -24,24 +26,7 @@ fun Application.configureDatabases(): DSLContext {
     println("Database successfully connected")
 
     val autoMigrate = environment.config.property("database.autoMigrate").getString().toBoolean()
-
-    try {
-        val flyway = Flyway.configure()
-            //.locations("classpath:db/migration")
-            .schemas("public")
-            .driver(driver)
-            .baselineOnMigrate(true)
-            .table("migrations")
-            .dataSource(dataSource)
-            .load()
-
-        if (autoMigrate) {
-            flyway.migrate()
-            println("Migration successfully executed")
-        }
-    }catch (e: Exception){
-        println("Error migration: ${e.message}")
-    }
+    migrate(dataSource, driver, autoMigrate)
 
     return DSL.using(dataSource, SQLDialect.POSTGRES)
 }
@@ -53,7 +38,40 @@ private fun createDataSource(url: String, user: String, password: String, driver
         this.password = password
         driverClassName = driver
     }
-    return HikariDataSource(config)
+    val dataSource = HikariDataSource(config)
+    dataSource.connection.use { conn ->
+        println(conn.metaData.databaseProductName)
+        println(conn.metaData.databaseProductVersion)
+    }
+    return dataSource
+}
+
+private fun migrate(dataSource: HikariDataSource, driver: String, autoMigrate : Boolean = true){
+
+    try {
+        val flyway = Flyway.configure()
+            //.locations("classpath:db/migration")
+            .dataSource(dataSource)
+            .schemas("public")
+            .driver(driver)
+            .baselineOnMigrate(true)
+            .validateOnMigrate(false)
+            .table("migrations")
+            .loggers("slf4j")
+            .load()
+
+
+        if (autoMigrate) {
+            flyway.migrate()
+            println("Migration successfully executed")
+        }
+    } catch (e: FlywayValidateException) {
+        println("⚠️ Migration validation failed: ${e.message}")
+    } catch (e: FlywayException) {
+        println("❌ General Flyway error: ${e.message}")
+    } catch (e: Exception){
+        println("Error migration: ${e.message}")
+    }
 }
 
 //private fun createDslContext(url: String, user: String, password: String): DSLContext {
